8495ed6755075a22fef03c4e68b66994b5938337
ZeBolHD <zezebolz@gmail.com>
2024-07-01 22:17:21 +0300

    chore(frontend): added new sections

--- apps/frontend/src/app/(site)/page.tsx
+++ apps/frontend/src/app/(site)/page.tsx
@@ -4,6 +4,9 @@ import AdvantagesSection from "./_components/AdvantagesSection";
 import CompaniesSection from "./_components/CompaniesSection";
 import HeroSection from "./_components/HeroSection";
 import ProgramSection from "./_components/ProgramSection";
+import FAQSection from "./_components/FAQSection";
+import PricingSection from "./_components/PricingSection";
+import GetStartedSection from "./_components/GetStartedSection";
 
 export default function HomePage() {
   return (
@@ -14,6 +17,9 @@ export default function HomePage() {
       <ProgramSection />
       <CompaniesSection />
       <AboutSection />
+      <FAQSection />
+      <PricingSection />
+      <GetStartedSection />
     </div>
   );
 }
