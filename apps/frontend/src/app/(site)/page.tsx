afdf71e3a776f35b8a5baae3ae9ed9ef4aa98838
ZeBolHD <zezebolz@gmail.com>
2024-07-01 18:43:12 +0300

    chore(frontend): added sections to main page

--- apps/frontend/src/app/(site)/page.tsx
+++ apps/frontend/src/app/(site)/page.tsx
@@ -1,3 +1,19 @@
+import BackgroundGradient from "./BackgroundGradient";
+import AboutSection from "./_components/AboutSection";
+import AdvantagesSection from "./_components/AdvantagesSection";
+import CompaniesSection from "./_components/CompaniesSection";
+import HeroSection from "./_components/HeroSection";
+import ProgramSection from "./_components/ProgramSection";
+
 export default function HomePage() {
-  return <div className="text-4xl">Test</div>;
+  return (
+    <div className="text-4xl">
+      <BackgroundGradient />
+      <HeroSection />
+      <AdvantagesSection />
+      <ProgramSection />
+      <CompaniesSection />
+      <AboutSection />
+    </div>
+  );
 }
