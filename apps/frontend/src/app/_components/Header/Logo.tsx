32ddb2fee34668f76ecbb7d1e97a187c6cd1b8cf
ZeBolHD <zezebolz@gmail.com>
2024-07-03 12:38:18 +0300

    style(frontend): removed logo from header

--- apps/frontend/src/app/_components/Header/Logo.tsx
+++ apps/frontend/src/app/_components/Header/Logo.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-
 const Logo = () => {
   return (
     <a className="flex items-center" href="/">
-      <Image src="/logo.svg" alt="logo" width={32} height={32} className="w-8 h-8 mr-3" />
       <h1 className="text-[22px] font-bold">krisqa</h1>
     </a>
   );
