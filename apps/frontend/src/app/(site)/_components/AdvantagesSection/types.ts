324d340dc764b4ebb29cc93f43dac9e4619675df
ZeBolHD <zezebolz@gmail.com>
2024-07-03 12:40:14 +0300

    feat(frontend): added new info

--- apps/frontend/src/app/(site)/_components/AdvantagesSection/types.ts
+++ apps/frontend/src/app/(site)/_components/AdvantagesSection/types.ts
@@ -4,7 +4,7 @@ export type Advantage = {
 };
 
 export const advantages: Advantage[] = [
-  { heading: "64 ученика", description: "Полностью завершили менторство и получили  оффер" },
+  { heading: "30+ учеников", description: "Полностью завершили менторство и получили  оффер" },
   { heading: "2-2,5 месяца", description: "Время прохождения всей программы " },
   { heading: "170 000 ₽", description: "Средняя зарплата учеников после  окончания занятий" },
 ];
