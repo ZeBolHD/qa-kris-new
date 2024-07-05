73d5b74f5e27c4baa60da8ae4b1e9a91c9bb7048
ZeBolHD <zezebolz@gmail.com>
2024-07-05 12:23:34 +0300

    style(frontend): removed offers

--- apps/frontend/src/app/(site)/_components/FAQSection/index.tsx
+++ apps/frontend/src/app/(site)/_components/FAQSection/index.tsx
@@ -6,17 +6,17 @@ import { offers, questions } from "./types";
 const FAQSection = () => {
   return (
     <section id="faq" className="py-[140px]">
-      <div className="container w-full flex justify-between relative">
+      <div className="container w-full relative">
         <BackgroundPatterns />
-        <div className="w-[480px] z-50">
-          <h2 className="text-[42px] font-bold">Вопросы и ответы</h2>
-          <div className="w-full mt-[60px]">
-            <QuestionList questions={questions} />
-          </div>
-        </div>
         <div className="z-50">
-          <Offers offers={offers} />
+          <h2 className="text-[42px] font-bold">Вопросы и ответы</h2>
         </div>
+        <div className="w-full mt-[60px]">
+          <QuestionList questions={questions} />
+        </div>
+        {/* <div className="z-50">
+          <Offers offers={offers} />
+        </div> */}
       </div>
     </section>
   );
