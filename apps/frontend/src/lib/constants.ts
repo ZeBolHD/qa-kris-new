25810b1faf426498a909c58f1944c72ad8f3a992
azinovev <zezebolz@gmail.com>
2024-12-14 12:07:02 +0300

    added public env

--- apps/frontend/src/lib/constants.ts
+++ apps/frontend/src/lib/constants.ts
@@ -3,5 +3,5 @@ export const BACKEND_URL = process.env.BACKEND_URL as string;
 
 export const REVALIDATE_KEY = process.env.REVALIDATE_KEY as string;
 
-export const TERMINAL_KEY = process.env.TERMINAL_KEY as string;
-export const TERMINAL_COMPANY_EMAIL = process.env.TERMINAL_COMPANY_KEY as string;
+export const TERMINAL_KEY = process.env.NEXT_PUBLIC_TERMINAL_KEY as string;
+export const TERMINAL_COMPANY_EMAIL = process.env.NEXT_PUBLIC_TERMINAL_COMPANY_KEY as string;
