81f176773dba0a423ca866bc90879aeed8737d35
azinovev <zezebolz@gmail.com>
2024-12-14 14:59:12 +0300

    fixed env key

--- apps/frontend/src/lib/constants.ts
+++ apps/frontend/src/lib/constants.ts
@@ -4,4 +4,4 @@ export const BACKEND_URL = process.env.BACKEND_URL as string;
 export const REVALIDATE_KEY = process.env.REVALIDATE_KEY as string;
 
 export const TERMINAL_KEY = process.env.NEXT_PUBLIC_TERMINAL_KEY as string;
-export const TERMINAL_COMPANY_EMAIL = process.env.NEXT_PUBLIC_TERMINAL_COMPANY_KEY as string;
+export const TERMINAL_COMPANY_EMAIL = process.env.NEXT_PUBLIC_TERMINAL_COMPANY_EMAIL as string;
